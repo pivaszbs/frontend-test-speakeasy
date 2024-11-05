9d441d93a4fc524a74937a5d3313fa63da57040a
Adam Bull <adam@speakeasy.com>
2024-11-05 12:01:03 -0800

    init

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import "./App.css";
+import { FuzzySearch } from "./FuzzySearch";
+function App() {
+  return (
+    <div className="flex flex-col items-center justify-center h-full">
+      <FuzzySearch />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/FuzzySearch.tsx
@@ -0,0 +1,7 @@
+interface FuzzySearchProps {
+  // Add some props
+}
+
+export function FuzzySearch(props: FuzzySearchProps) {
+  return <div>Implement me</div>;
+}
